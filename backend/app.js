ca6c6ca060a91d3afdce492b8dcf22ddb10c9890
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-02-09 23:39:02 +0500

    return app and middlewares

---
+++ backend/app.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoose = require('mongoose');
+const { celebrate, Joi, errors } = require('celebrate');
+const isUrl = require('validator/lib/isURL');
+const BadRequest = require('./errors/400-BadRequestError');
+const { createUser, login } = require('./controllers/users');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
+const handleError = require('./middlewares/handleError');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+
+mongoose.connect(MONGO_URL);
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Слишком много запросов, пожалуйста попробуйте позже :)',
+});
+
+app.use(cookieParser());
+app.use(limiter);
+app.use(helmet());
+
+app.use(express.json());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom((link) => {
+      if (isUrl(link, { require_protocol: true })) {
+        return link;
+      }
+      throw new BadRequest('Некорректный адрес URL');
+    }),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.use(auth);
+
+app.use('/cards', require('./routes/cards'));
+app.use('/users', require('./routes/users'));
+
+app.use('/', require('./routes/notExist'));
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use(handleError);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
