776dbcc76507382895b98ab9934e5db56ed80bc3
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-02-10 00:00:46 +0500

    del backend

--- backend/controllers/notExist.js
+++
@@ -1,5 +0,0 @@
-const NotFoundError = require('../errors/404-NotFoundError');
-
-exports.notExist = (req, res, next) => {
-  next(new NotFoundError('Веб-страница ищет HTML своей жизни. Желательно без ошибок и вредных привычек :)'));
-};
--- backend/routes/notExist.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { notExist } = require('../controllers/notExist');
-
-router.all('/*', notExist);
-
-module.exports = router;
