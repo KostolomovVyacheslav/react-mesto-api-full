4f358b7a9acfaf3f155fe50724e79a6625cbf925
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-02-11 22:04:31 +0500

    access all

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,8 +11,8 @@ const cors = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
