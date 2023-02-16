e6711a1599dab6b72ab147540cad3141a58c3c36
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-02-16 23:33:11 +0500

    fix: limeter config

--- backend/utils/limiterConfig.js
+++ backend/utils/limiterConfig.js
@@ -2,11 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  // max: 100,
-  max: 2,
+  max: 100,
   message: 'Слишком много запросов, пожалуйста попробуйте позже :)',
 });
 
-module.exports = {
-  limiter,
-};
+module.exports = limiter;
