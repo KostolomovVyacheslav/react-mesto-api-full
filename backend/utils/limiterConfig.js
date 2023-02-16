80ef2279504ca4cfe0895db700e49b642df324bd
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2023-02-16 23:53:18 +0500

    fix: change limiterConfig options

--- backend/utils/limiterConfig.js
+++ backend/utils/limiterConfig.js
@@ -2,8 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  // max: 100,
-  max: 15,
+  max: 100,
   message: 'Слишком много запросов, пожалуйста попробуйте позже :)',
 });
 
